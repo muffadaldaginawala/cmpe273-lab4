e87f4e51deaca2f0001ab796da6afe80afeb8cd9
Muffadal Daginawala <muffi.daginawala@gmail.com>
2014-12-21 00:09:29 -0800

    function call change

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -3,7 +3,7 @@ package edu.sjsu.cmpe.cache.client;
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Cache Service Start ");
+        System.out.println("Cache Service Starting... ");
         CacheServiceInterface cache = new DistributedCacheService(
                 "http://localhost:3000");
         CacheServiceInterface myCache = new DistributedCacheService(
@@ -11,15 +11,15 @@ public class Client {
 
 
         System.out.println("putting (1 => a)");
-        myCache.asyncWrite(1, "a");
+        myCache.asyncPUT(1, "a");
         System.out.println("Sleeping for 30 seconds...");
         Thread.sleep(30000);
         System.out.println("Updating putting (1 => b)");
-        myCache.asyncWrite(1, "b");
+        myCache.asyncPUT(1, "b");
         System.out.println("Sleeping for 30 seconds...");
         Thread.sleep(30000);
         System.out.println("getting values...");
-        String value = myCache.asyncRead(1);
+        String value = myCache.asyncGET(1);
         System.out.println("Value received: "+value);
         System.out.println("Existing Cache Client...");
     }
